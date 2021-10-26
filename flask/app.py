096dd832f9748940b3537251b6a655c1ad61dea2
Esp-v2 <projectdaruma@gmail.com>
2021-10-27 07:04:21 +0900

    未完成

--- flask/app.py
+++ flask/app.py
@@ -10,8 +10,15 @@ def index():
 
 def prediction():
     while True:
-        pred = get_pred()
-        yield pred
+        pred = str(get_pred())
+        yield (b'--frame\r\n'
+                  b'Content-type: text/html\n' + pred + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+   #imgタグに埋め込まれるResponseオブジェクトを返す
+   return Response(prediction(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == '__main__':
   app.run()
